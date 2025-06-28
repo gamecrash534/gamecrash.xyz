16e8db8fce36c0602ff6340259ccd6fbe2455382
game.crash <73080127+gamecrash534@users.noreply.github.com>
2025-06-28 18:50:11 +0200

    feat: enhance about page with devicon integration and add custom color classes

--- js/include.js
+++ js/include.js
@@ -4,7 +4,7 @@
     bootstrap.src = "https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/js/bootstrap.bundle.min.js";
     bootstrap.defer = true;
     var bootstrapIcons = document.createElement("script");
-    bootstrapIcons.src = "https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.js";
+    bootstrapIcons.src = "https://cdn.jsdelivr.net/npm/bootstrap-icons@1.13.1/font/bootstrap-icons.js";
     bootstrapIcons.defer = true;
     document.body.appendChild(bootstrap);
     document.body.appendChild(bootstrapIcons);
